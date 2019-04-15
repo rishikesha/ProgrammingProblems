69fe4d9606bb585943754a5f2eeb239ab07de2b3
Rishikesh <rishikes@gmail.com>
2019-04-15 13:12:00 -0700

    working uva 1089

--- uva1089.cpp
+++ uva1089.cpp
@@ -7,41 +7,43 @@
 using namespace std;
 
 void processMatrix(int m, int n) {
-        vector<vector<int>> transpM(n+1), transpP(n+1);
+    string s;
+    istringstream iss;
 
-        for(int i = 1; i <= m; i++) {
-            vector<int> position;
+    vector<vector<int>> transpM(n+1), transpP(n+1);
 
-            getline(cin,s); iss = istringstream(s);
-            int ct;
-            iss >> ct;
-            for (int j = 0; j < ct; j++) {
-                int tmp; iss>> tmp;
-                position.push_back(tmp);
-                transpP[tmp].push_back(i);
-            }
+    for(int i = 1; i <= m; i++) {
+        vector<int> position;
 
-            getline(cin,s); iss = istringstream(s);
-            for (int j = 0; j < ct; j++) {
-                int tmp; iss>> tmp;
-                transpM[position[j]].push_back(tmp);
-            }
+        getline(cin,s); iss = istringstream(s);
+        int ct;
+        iss >> ct;
+        for (int j = 0; j < ct; j++) {
+            int tmp; iss>> tmp;
+            position.push_back(tmp);
+            transpP[tmp].push_back(i);
         }
 
-        cout << n << " " <<m << endl;
-        for(int i=1; i <= n; i++) {
-            cout << transpP[i].size();
-            for(auto dd : transpP[i]) {
-                cout << " " << dd;
-            }
-            cout << endl;
-            for(int k = 0; k < transpM[i].size(); k++) {
-                if (k != 0)
-                    cout << " ";
-                cout  << transpM[i][k];
-            }
-            cout << endl;
+        getline(cin,s); iss = istringstream(s);
+        for (int j = 0; j < ct; j++) {
+            int tmp; iss>> tmp;
+            transpM[position[j]].push_back(tmp);
         }
+    }
+
+    cout << n << " " <<m << endl;
+    for(int i=1; i <= n; i++) {
+        cout << transpP[i].size();
+        for(auto dd : transpP[i]) {
+            cout << " " << dd;
+        }
+        cout << endl;
+        for(int k = 0; k < transpM[i].size(); k++) {
+            if (k != 0)
+                cout << " ";
+            cout  << transpM[i][k];
+        }
+        cout << endl;
     }
 }
 
