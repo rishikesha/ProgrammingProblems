523e5602fa7f410ebcf691f4a4d00b3bfaf5033a
Rishikesh <rishikes@gmail.com>
2021-05-06 09:34:41 -0700

    an easy problem with edge cases one can overlook

---
+++ Kattis/moscowdream.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import os, sys
+
+# Moscow Dream
+# Everything is obvious except that we have to check
+# there are adequate number of problems, and the number
+# of problems is at least 3
+#
+
+if __name__ == "__main__":
+    f = open(sys.argv[1]) if "TESTR" in os.environ  else sys.stdin
+    a,b,c,n = map(int, f.readline().split())
+
+    if a > 0 and b > 0 and c > 0 and a+b+c >= n and n >= 3:
+        print("YES\n")
+    else:
+        print("NO\n")
+
